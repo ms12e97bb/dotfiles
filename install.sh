a2786771ca71c3eb233075cfe966a8842cd04cc4
ms12e97bb <ms.12e97bb@gmail.com>
2023-01-15 22:53:48 +0900

    mod install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 cp .bash_profile ~/
 cp .vimrc ~/
 cp .screenrc ~/
